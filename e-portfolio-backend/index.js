fb3a1099ecacb5f08b8750022d0e4ff2b263c75d
louis Lazare <louislazare.pro@gmail.com>
2024-07-01 20:56:42 +0200

    [GIT]

--- e-portfolio-backend/index.js
+++ e-portfolio-backend/index.js
@@ -2,9 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// Middleware
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -18,7 +20,6 @@ app.use('/api/personal-info', personalInfoRouter);
 app.use('/api/skills', skillsRouter);
 app.use('/api/projects', projectsRouter);
 
-// Serve static files (optional, if needed)
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, () => {
