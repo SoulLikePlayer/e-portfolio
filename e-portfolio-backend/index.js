9f9d07fc29496394885540e53a4a89eda9be7015
louis Lazare <louislazare.pro@gmail.com>
2024-08-11 00:05:06 +0200

    [RESET]

--- e-portfolio-backend/index.js
+++ e-portfolio-backend/index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -19,4 +20,6 @@ app.use('/api/project', projectsRouter);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-module.exports = app;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
