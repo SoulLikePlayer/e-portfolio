210b26c881e917dd2810fe2182faa9de3aaa2628
Louis Lazare <lazare231@etu.unicaen.fr>
2024-09-03 16:17:33 +0200

    [UPDATE]: Amélioration du Content.js

--- src/components/pages/Content.jsx
+++ src/components/pages/Content.jsx
@@ -3,24 +3,29 @@ import '../../styles/style.css';
 import '../../styles/square.css';
 import '../../styles/neon.css';
 import '../../styles/animation-article.css';
-import { motion } from "framer-motion";
+
+import { motion } from 'framer-motion';
 import { Typewriter } from 'react-simple-typewriter';
 import { articlesData } from '../data/article';
+import { FaArrowDown, FaArrowUp } from 'react-icons/fa';
+import { useMediaQuery } from 'react-responsive';
 
 function Content({ onContentChange }) {
   const [articles, setArticles] = useState(articlesData);
   const [transitioning, setTransitioning] = useState(null);
+  const [showConfetti, setShowConfetti] = useState(false);
+  const isSmallScreen = useMediaQuery({ query: '(max-width: 768px)' });
 
   useEffect(() => {
     const neonBorders = document.querySelectorAll('.neon-border');
 
-    function getRandomHSL() {
+    const getRandomHSL = () => {
       const hue = Math.floor(Math.random() * 360);
       return `hsl(${hue}, 100%, 50%)`;
-    }
+    };
 
-    function changeNeonColors() {
-      neonBorders.forEach(border => {
+    const changeNeonColors = () => {
+      neonBorders.forEach((border) => {
         let currentHue = parseInt(border.dataset.hue, 10);
         currentHue = (currentHue + 1) % 360;
         const newColor = `hsl(${currentHue}, 100%, 50%)`;
@@ -32,9 +37,9 @@ function Content({ onContentChange }) {
 
         border.dataset.hue = currentHue;
       });
-    }
+    };
 
-    neonBorders.forEach(border => {
+    neonBorders.forEach((border) => {
       const color = getRandomHSL();
       border.style.borderColor = color;
       border.style.boxShadow = `0 0 10px ${color}, 0 0 20px ${color}, 0 0 30px ${color}`;
@@ -43,36 +48,59 @@ function Content({ onContentChange }) {
 
     const colorInterval = setInterval(changeNeonColors, 50);
 
-    return () => {
-      clearInterval(colorInterval);
-    };
+    return () => clearInterval(colorInterval);
   }, []);
 
   const toggleArticleContent = (index) => {
     setTransitioning(index);
+    setShowConfetti(true);
     setTimeout(() => {
-      setArticles(prevArticles => {
+      setArticles((prevArticles) => {
         const newArticles = [...prevArticles];
         const currentArticle = newArticles[index];
 
         switch (currentArticle.id) {
           case 'biographie':
-            newArticles[index] = { id: 'competence', title: 'Mes compétences', content: 'Découvrez les différentes compétence apprise au long de mes experience' };
+            newArticles[index] = {
+              id: 'competence',
+              title: 'Mes compétences',
+              content: 'Découvrez les différentes compétences acquises au fil de mes expériences.',
+            };
             break;
           case 'competence':
-            newArticles[index] = { id: 'project', title: 'Mes projets', content: 'Découvrez les différents projets de développement fait' };
+            newArticles[index] = {
+              id: 'project',
+              title: 'Mes projets',
+              content: 'Découvrez les différents projets de développement réalisés.',
+            };
             break;
           case 'project':
-            newArticles[index] = { id: 'biographie', title: 'Qui suis-je ?', content: 'Découvrez qui je suis, ma personnalité, mon github, mon numéro de téléphone, etc...' };
+            newArticles[index] = {
+              id: 'biographie',
+              title: 'Qui suis-je ?',
+              content: 'Découvrez qui je suis, ma personnalité, mon GitHub, mon numéro de téléphone, etc...',
+            };
             break;
           case 'etude':
-            newArticles[index] = { id: 'parcours', title: 'Mon Parcours', content: 'Découvrez mon parcours à travers une frise chronologique.' };
+            newArticles[index] = {
+              id: 'parcours',
+              title: 'Mon Parcours',
+              content: 'Découvrez mon parcours à travers une frise chronologique.',
+            };
             break;
           case 'parcours':
-            newArticles[index] = { id: 'localisation', title: 'Où je vis ?', content: 'Découvrez ma localisation.' };
-            break;  
+            newArticles[index] = {
+              id: 'localisation',
+              title: 'Où je vis ?',
+              content: 'Découvrez ma localisation.',
+            };
+            break;
           case 'localisation':
-            newArticles[index] = { id: 'etude', title: 'Que fais-je ?', content: 'Description de l\'article 2.' };
+            newArticles[index] = {
+              id: 'etude',
+              title: 'Que fais-je ?',
+              content: 'Découvrez mes études et recherches en cours.',
+            };
             break;
           default:
             break;
@@ -80,7 +108,8 @@ function Content({ onContentChange }) {
         return newArticles;
       });
       setTransitioning(null);
-    }, 500); // Durée de l'effet de transition
+      setShowConfetti(false);
+    }, 500); // Transition duration
   };
 
   const handleArticleClick = (index) => {
@@ -93,18 +122,22 @@ function Content({ onContentChange }) {
       initial={{ opacity: 0, filter: 'blur(10px)' }}
       animate={{ opacity: 1, filter: 'blur(0px)' }}
       exit={{ opacity: 0, filter: 'blur(10px)' }}
-      transition={{ duration: 0.5, ease: 'easeInOut' }}>
-      
+      transition={{ duration: 0.5, ease: 'easeInOut' }}
+    >
       <section className="intro-section text-center py-5">
         <h2 className="text-4xl mb-4 eportfolio">
           <Typewriter words={['Bienvenue sur mon e-portfolio']} />
         </h2>
         <p className="text-gray-400">
+          <span>Découvrez </span>
           <Typewriter
-            words={['Découvrez mon parcours, mes compétences et mes projets.']}
+            words={['mon parcours', 'mes compétences', 'mes projets', 'mon profil']}
+            loop={true}
             cursor
             cursorStyle="|"
-            loop
+            typeSpeed={80}
+            deleteSpeed={50}
+            delaySpeed={1000}
           />
         </p>
       </section>
@@ -112,9 +145,17 @@ function Content({ onContentChange }) {
       <section className="my-8">
         <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-3 gap-3 mt-3 -mx-3">
           {articles.map((article, index) => (
-            <div className="article-container" key={index}>
+            <motion.div
+              className="article-container"
+              key={index}
+              initial={{ scale: 0.9 }}
+              animate={{ scale: 1 }}
+              transition={{ duration: 0.3 }}
+            >
               <div
-                className={`bg-white p-4 rounded shadow-md neon-border animated-article article${index + 1} h-full ${transitioning === index ? 'article-hidden' : ''}`}
+                className={`bg-white p-4 rounded shadow-md neon-border animated-article article${index + 1} h-full ${
+                  transitioning === index ? 'article-hidden' : ''
+                }`}
                 data-content={article.id}
                 onClick={() => handleArticleClick(index)}
               >
@@ -123,8 +164,14 @@ function Content({ onContentChange }) {
                   <p className="text-gray-600">{article.content}</p>
                 </div>
               </div>
-              <button id={`switch-article${index + 1}`} className="down-arrow" onClick={() => toggleArticleContent(index)}>▼</button>
-            </div>
+              <button
+                id={`switch-article${index + 1}`}
+                className="down-arrow"
+                onClick={() => toggleArticleContent(index)}
+              >
+                <FaArrowDown />
+              </button>
+            </motion.div>
           ))}
         </div>
       </section>
