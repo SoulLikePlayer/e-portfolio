e8b65eb648461eee63b6b711652f76d7d3c96ddb
louis Lazare <louislazare.pro@gmail.com>
2025-01-03 23:12:50 +0100

    [FEAT] : Portage du serveur sur GitHub

--- src/components/data/article.js
+++
@@ -1,5 +0,0 @@
-export const articlesData = [
-  { id: 'biographie', title: 'Qui suis-je ?', content: 'Découvrez qui je suis, ma personnalité, mon github, mon numéro de téléphone, etc...' },
-  { id: 'etude', title: 'Mes études actuelles', content: 'Découvrez les formations et diplômes que je poursuis actuellement, ainsi que les compétences développées à travers mes études.' },
-  { id: 'hobbit', title: 'Mes passions', content: 'Description de l\'article 3.' },
-];
--- src/components/data/bio.js
+++
@@ -1,7 +0,0 @@
-export const data = {
-    name: "Louis Lazare",
-    email: "louislazare.pro@gmail.com",
-    phone: "+33783105098",
-    github: "https://github.com/SoulLikePlayer/",
-    photo: "../img/profil.png"
-  };
