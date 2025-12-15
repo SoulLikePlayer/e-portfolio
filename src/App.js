e8cda3e29e5473dcf912b46253e3505df03211df
Louis LAZARE <louislazare.pro@gmail.com>
2025-12-15 22:50:21 +0100

    [FEAT] : Add tailwind configuration

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 function App() {
     return (
     <Router basename="/">
-      <div>
+      <div className="App">
       </div>
     </Router>
   );
