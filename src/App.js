afd84464e09052093fd95f25aed90fcc16e24609
Louis LAZARE <louislazare.pro@gmail.com>
2025-12-15 23:12:39 +0100

    [FEAT] : Add first version of navbar without using router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React, { useState, useEffect, Suspense, lazy, useRef } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-
+import Navbar from './components/navbar';
 
 function App() {
     return (
     <Router basename="/">
       <div className="App">
+        <header>
+          <Navbar />
+        </header>
+        <main>
+
+        </main>
+        <footer>
+          
+        </footer>
       </div>
     </Router>
   );
