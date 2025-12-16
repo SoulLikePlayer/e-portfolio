9a0261bc463584c3dae8902f810deccaccf38a87
Louis LAZARE <louislazare.pro@gmail.com>
2025-12-16 12:55:54 +0100

    [FEAT] : Add start

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,25 +3,24 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
     return (  
-    <nav class="bg-neutral-primary fixed w-full z-20 top-0 start-0 border-b border-default">
-    <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a class="flex items-center space-x-3 rtl:space-x-reverse">
-            <Link to="/" class="self-center text-xl text-heading font-semibold whitespace-nowrap">Mon E-Portfolio</Link>
-        </a>
-        <button data-collapse-toggle="navbar-solid" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-body rounded-base md:hidden hover:bg-neutral-secondary-soft hover:text-heading focus:outline-none focus:ring-2 focus:ring-neutral-tertiary" aria-controls="navbar-solid" aria-expanded="false">
-            <span class="sr-only">Open main menu</span>
-            <svg class="w-6 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M5 7h14M5 12h14M5 17h14"/></svg>
-        </button>
-        <div class="hidden w-full md:block md:w-auto" id="navbar-default">
-        <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-default rounded-base bg-neutral-secondary-soft md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-neutral-primary">
-            <li>
-                <a>Qui-suis je ?</a>
-            </li>
-        </ul>
+    <nav className="bg-neutral-primary fixed w-full z-20 top-0 start-0 border-b border-default">
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+            <span className="flex items-center space-x-3 rtl:space-x-reverse">
+                <Link to="/" className="self-center text-xl text-heading font-semibold whitespace-nowrap">Mon E-Portfolio</Link>
+            </span>
+            <button data-collapse-toggle="navbar-solid" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-body rounded-base md:hidden hover:bg-neutral-secondary-soft hover:text-heading focus:outline-none focus:ring-2 focus:ring-neutral-tertiary" aria-controls="navbar-solid" aria-expanded="false">
+                <span className="sr-only">Open main menu</span>
+                <svg className="w-6 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M5 7h14M5 12h14M5 17h14"/></svg>
+            </button>
+            <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+            <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-default rounded-base bg-neutral-secondary-soft md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-neutral-primary">
+                <li>
+                    <Link to="/">Qui-suis je ?</Link>
+                </li>
+            </ul>
+            </div>
         </div>
-    </div>
     </nav>
-
     );
 }
 
